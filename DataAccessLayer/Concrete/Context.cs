37e7eb855a6f67cc152a2d5ba20a17b8caaa1687
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-02-26 17:03:02 +0300

    DB Context Class Created.

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,20 @@ namespace DataAccessLayer.Concrete
 {
     public class Context : DbContext
     {
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=NGNCNB842;database=NFTCollectionMakerDB;integrated security=true;");
+        }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Artwork> Artworks { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<CollectionAnalytic> CollectionAnalytics { get; set; }
+        public DbSet<CollectionLayer> CollectionLayers { get; set; }
+        public DbSet<ArtworkLayer> ArtworkLayers { get; set; }
+        public DbSet<ArtworkTag> ArtworkTags { get; set; }
+        public DbSet<LayerTag> LayerTags { get; set; }
+        public DbSet<LayerType> LayerTypes { get; set; }
     }
 }
