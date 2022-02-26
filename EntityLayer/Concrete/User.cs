b6988e5d8aea24dbb4eebb3bc322dcd4b5a1ff21
ibrahim.cagirkan <ibrahim.cagirkan@NGNCNB842>
2022-02-26 14:26:27 +0300

    Add project files.

---
+++ EntityLayer/Concrete/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
