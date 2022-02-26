c0a7efdebdfb8c13fc205947be0fe2b60785690c
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-02-26 15:14:09 +0300

    Artwork Entity Created

---
+++ EntityLayer/Concrete/Artwork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    class Artwork
+    {
+        [Key]
+        public int ArtworkID { get; set; }
+        [StringLength(50)]
+        public string ArtworkName { get; set; }
+        [StringLength(512)]
+        public string ImageURL { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
