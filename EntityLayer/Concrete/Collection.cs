e02c277568c3c4aa37afa769eb364ed8cd0c8e63
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-02-26 15:06:38 +0300

    EF Core added, Collection Entity

---
+++ EntityLayer/Concrete/Collection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    class Collection
+    {
+        [Key]
+        public int CollectionID { get; set; }
+        [StringLength(50)]
+        public string CollectionName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
