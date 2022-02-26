91e5738cd3fe2c84ea8c254d3b4d44df6c3c33d3
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-02-26 15:39:28 +0300

    LayerTag Entity Created

---
+++ EntityLayer/Concrete/LayerTag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    class LayerTag
+    {
+        public int LayerTagID { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
