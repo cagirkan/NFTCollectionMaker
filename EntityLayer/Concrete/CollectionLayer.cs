6b11904a08080f3e4d806c7f93ef55da415a7a56
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-02-26 15:33:38 +0300

    CollectionLayer Entity Created

---
+++ EntityLayer/Concrete/CollectionLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    class CollectionLayer
+    {
+        [Key]
+        public int CollectionLayerID { get; set; }
+        [StringLength(50)]
+        public string CollectionLayerName { get; set; }
+        [StringLength(512)]
+        public string ImageURL { get; set; }
+        public int LayerIndex { get; set; }
+        public int Popularity { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
