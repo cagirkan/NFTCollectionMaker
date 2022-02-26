141d6203f5ab5b5dbe2055fc902c79d4e2812950
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-02-26 15:28:28 +0300

    LayerType Entity Created

---
+++ EntityLayer/Concrete/LayerType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    class LayerType
+    {
+        [Key]
+        public int LayerTypeID { get; set; }
+        [StringLength(25)]
+        public string LayerTypeName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
