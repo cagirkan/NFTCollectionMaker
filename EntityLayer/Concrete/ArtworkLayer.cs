5b13367081cf09e3931487acedabed444174fb8a
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-02-26 15:37:35 +0300

    ArtworkLayer Entity Created

---
+++ EntityLayer/Concrete/ArtworkLayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+    class ArtworkLayer
+    {
+        [Key]
+        public int ArtworkLayerID { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EntityLayer.Concrete
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
