a87f59eb68a6e95d69db343a1ec23c1307d2a6dd
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-02-26 14:45:48 +0300

    Dependencies added, User Entity Created

--- EntityLayer/Concrete/User.cs
+++ EntityLayer/Concrete/User.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EntityLayer.Concrete
 {
     class User
     {
+        [Key]
         public int UserID { get; set; }
+        [StringLength(25)]
         public string UserName { get; set; }
+        [StringLength(50)]
         public string Email { get; set; }
         public string Password { get; set; }
         public DateTime CreatedAt { get; set; }
