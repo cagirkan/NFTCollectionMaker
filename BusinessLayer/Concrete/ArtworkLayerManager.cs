87edf456fe906626029457d60af88c647c5667e8
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-03-20 14:50:30 +0300

    ArtworkLayer BL and Endpoints

---
+++ BusinessLayer/Concrete/ArtworkLayerManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Concrete
+{
+    public class ArtworkLayerManager : IArtworkLayerService
+    {
+        IArtworkLayerDal _artworkLayer;
+
+        public ArtworkLayerManager(IArtworkLayerDal artworkLayer)
+        {
+            _artworkLayer = artworkLayer;
+        }
+
+        public void Add(ArtworkLayer t)
+        {
+            _artworkLayer.Insert(t);
+        }
+
+        public void Delete(ArtworkLayer t)
+        {
+            _artworkLayer.Delete(t);
+        }
+
+        public ArtworkLayer GetByID(int id)
+        {
+            return _artworkLayer.GetByID(id);
+        }
+
+        public List<ArtworkLayer> GetList()
+        {
+            return _artworkLayer.GetListAll();
+        }
+
+        public void Update(ArtworkLayer t)
+        {
+            _artworkLayer.Update(t);
+        }
+    }
+}
---
+++ NFTCollectionMakerAPI/Controllers/ArtworkLayersController.cs
@@ -0,0 +1,66 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace NFTCollectionMakerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtworkLayersController : ControllerBase
+    {
+        ArtworkLayerManager alm = new ArtworkLayerManager(new EfArtworkLayerRepository());
+        [HttpGet]
+        public IActionResult GetTags()
+        {
+            var artworkLayers = alm.GetList();
+            return Ok(artworkLayers);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetTags(int id)
+        {
+            var artworkLayer = alm.GetByID(id);
+            if (artworkLayer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(artworkLayer);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateTag(ArtworkLayer artworkLayer)
+        {
+            artworkLayer.CreatedAt = DateTime.Now;
+            alm.Add(artworkLayer);
+            return StatusCode(StatusCodes.Status201Created, artworkLayer.ArtworkLayerID);
+        }
+
+        [HttpPut]
+        public IActionResult EditTag(ArtworkLayer artworkLayer)
+        {
+            alm.Update(artworkLayer);
+            return StatusCode(StatusCodes.Status202Accepted, artworkLayer);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteTag(int id)
+        {
+            var artworkLayer = alm.GetByID(id);
+            if (artworkLayer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                alm.Delete(artworkLayer);
+                return Ok();
+            }
+        }
+    }
+}
