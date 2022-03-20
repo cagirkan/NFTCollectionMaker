94e90576bcd328a398a4272193b91f79c9c61639
Halil İbrahim ÇAĞIRKAN <hicagirkan@gmail.com>
2022-03-20 14:30:34 +0300

    Layer Tag BL and Endpoints

---
+++ BusinessLayer/Concrete/LayerTagManager.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Concrete
+{
+    public class LayerTagManager : ILayerTagService
+    {
+        ILayerTagDal _layerTag;
+
+        public LayerTagManager(ILayerTagDal layerTag)
+        {
+            _layerTag = layerTag;
+        }
+
+        public void Add(LayerTag t)
+        {
+            _layerTag.Insert(t);
+        }
+
+        public void Delete(LayerTag t)
+        {
+            _layerTag.Delete(t);
+        }
+
+        public LayerTag GetByID(int id)
+        {
+            return _layerTag.GetByID(id);
+        }
+
+        public List<LayerTag> GetList()
+        {
+            return _layerTag.GetListAll();
+        }
+
+        public void Update(LayerTag t)
+        {
+            _layerTag.Update(t);
+        }
+    }
+}
---
+++ NFTCollectionMakerAPI/Controllers/LayerTagsController.cs
@@ -0,0 +1,66 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace NFTCollectionMakerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LayerTagsController : ControllerBase
+    {
+        LayerTagManager ltm = new LayerTagManager(new EfLayerTagRepository());
+        [HttpGet]
+        public IActionResult GetTags()
+        {
+            var layerTags = ltm.GetList();
+            return Ok(layerTags);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetTags(int id)
+        {
+            var layerTag = ltm.GetByID(id);
+            if (layerTag == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(layerTag);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateTag(LayerTag layerTag)
+        {
+            layerTag.CreatedAt = DateTime.Now;
+            ltm.Add(layerTag);
+            return StatusCode(StatusCodes.Status201Created, layerTag.TagID);
+        }
+
+        [HttpPut]
+        public IActionResult EditTag(LayerTag layerTag)
+        {
+            ltm.Update(layerTag);
+            return StatusCode(StatusCodes.Status201Created, layerTag);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteTag(int id)
+        {
+            var layerTag = ltm.GetByID(id);
+            if (layerTag == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                ltm.Delete(layerTag);
+                return Ok();
+            }
+        }
+    }
+}
